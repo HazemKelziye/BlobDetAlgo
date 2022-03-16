f3d751b552a2b43ab8766308581778522ff7d931
Hazem Kelziye <flashrassengan4@gmail.com>
2022-03-16 18:30:41 +0300

    Blob Detection Algorithm in C++

--- BlobDetAlgo/main.cpp
+++ BlobDetAlgo/main.cpp
@@ -1,14 +1,21 @@
-//
-//  main.cpp
-//  BlobDetAlgo
-//
-//  Created by hazem  kilzieh on 16.03.2022.
-//
-
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Mat Blob = imread("/Users/hazemkilzieh/Desktop/BlobDetAlgo/PartialBlob2.jpg",IMREAD_GRAYSCALE);
+    
+    
+    
+    for (int r = 0; r < Blob.rows; r++) {
+        for (int c = 0; c < Blob.cols; c++) {
+            <#statements#>
+        }
+    }
+    
     return 0;
 }
