d040d82876c44a534afa49b7249ba3f227f2965f
HaZem <flashrassengan4@gmail.com>
2022-03-16 18:12:52 +0300

    Initial Commit

---
+++ BlobDetAlgo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BlobDetAlgo
+//
+//  Created by hazem  kilzieh on 16.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
