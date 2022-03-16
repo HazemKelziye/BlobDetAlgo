bb80c7273ec9574b04b9cd48bb1d6cc5a57634d4
Hazem Kelziye <flashrassengan4@gmail.com>
2022-03-16 19:26:37 +0300

    Segmented Blobs added and detected their Center points individually

--- BlobDetAlgo/main.cpp
+++ BlobDetAlgo/main.cpp
@@ -7,7 +7,8 @@ using namespace cv;
 
 int main(int argc, const char * argv[]) {
 
-    Mat Blob = imread("/Users/hazemkilzieh/Desktop/BlobDetAlgo/PartialBlob2.jpg",IMREAD_GRAYSCALE);
+    Mat Blob = imread("/Users/hazemkilzieh/Desktop/BlobDetAlgo/Blob4.jpg",IMREAD_GRAYSCALE);
+    
     
     for (int r = 0; r < Blob.rows; r++) {
         for (int c = 0; c < Blob.cols; c++) {
@@ -17,23 +18,36 @@ int main(int argc, const char * argv[]) {
     }
     
     float u = 0, v = 0;
-    float uTempNumerator = 0, uTempDenominator = 0;
+    float uTempNumerator = 0, TempDenominator = 0, vTempNumerator = 0;
     
     for (int r = 0; r < Blob.rows; r++) {
         for (int c = 0; c < Blob.cols; c++) {
             uTempNumerator += (r*Blob.at<uint8_t>(r,c));
-            uTempDenominator += Blob.at<uint8_t>(r,c);
+            TempDenominator += Blob.at<uint8_t>(r,c);
+            vTempNumerator += (c*Blob.at<uint8_t>(r,c));
         }
     }
     
-    u = uTempNumerator/uTempDenominator;
-    
-    /*imshow("Blob", Blob);
-    waitKey();*/
+    u = uTempNumerator/TempDenominator;
+    v = vTempNumerator/TempDenominator;
     
     cout<<"\n\n"<<"This is the Whole Matrix of the image\n"<<Blob<<endl;
     cout<<"This is the 1st column\n"<<Blob.col(0)<<endl;
-    cout<<"This is the u of Blob "<<u<<endl;
+    cout<<"The sum of pixel values in 1st column: "<<sum(Blob.col(0))[0]<<endl;
+    cout<<"The number of White pixels in 1st columns:"<<sum(Blob.col(0))[0]/255<<endl;
+    cout<<"The number of Rows in the image:"<<Blob.rows<<endl;
+    cout<<"This is the u of Blob "<<u<<endl<<endl;
+    
+    cout<<"The sum of pixel values in the 40th row: "<<sum(Blob.row(39))[0]<<endl;
+    cout<<"The number of White pixels in 40th row:"<<sum(Blob.row(39))[0]/255<<endl;
+    cout<<"The number of Columns in the image:"<<Blob.cols<<endl;
+    cout<<"This is the v of Blob:"<<v<<endl<<"-----------------------------------------------------"<<endl;
+    
+    Blob.at<uint8_t>(u,v) = 0;
+    
+    namedWindow("Blob", WINDOW_FREERATIO);
+    imshow("Blob", Blob);
+    waitKey();
     
     return 0;
 }
